3bea1e888caf12ee7d6e3e133b0fdeafb7361bf6
Fitz Ryland <fitzryland@gmail.com>
2020-07-26 08:56:42 -0700

    some basic tests with lcd_test.py

--- lcd_test.py
+++ lcd_test.py
@@ -8,21 +8,30 @@ i2c = busio.I2C(board.SCL, board.SDA)
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 
 lcd.cursor = True
+mode = 'idle'
+# lcd.message = 'Press the green button';
 
 while True:
-	lcd.clear()
-	lcd.message = "Blue"
-	lcd.color = [0, 0, 100]
-	time.sleep(5)
-	lcd.clear()
-	lcd.message = "Pink!"
-	lcd.color = [100, 0, 100]
-	time.sleep(5)
-	lcd.clear()
-	lcd.message = "Yellow"
-	lcd.color = [100, 100, 0]
-	time.sleep(5)
-	lcd.clear()
-	lcd.message = "Full White"
-	lcd.color = [100, 100, 100]
-	time.sleep(5)
+    if mode == "idle":
+        if lcd.right_button:
+            lcd.clear()
+            lcd.message = "Right!"
+        elif lcd.left_button:
+            lcd.clear()
+            lcd.message = "Left!"
+    # lcd.clear()
+    # lcd.message = "Blue"
+    # lcd.color = [0, 0, 100]
+    # time.sleep(5)
+    # lcd.clear()
+    # lcd.message = "Pink!"
+    # lcd.color = [100, 0, 100]
+    # time.sleep(5)
+    # lcd.clear()
+    # lcd.message = "Yellow"
+    # lcd.color = [100, 100, 0]
+    # time.sleep(5)
+    # lcd.clear()
+    # lcd.message = "Full White"
+    # lcd.color = [100, 100, 100]
+    # time.sleep(5)
